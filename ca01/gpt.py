d22880d58869475457a2edcf4828a184979715b8
Brooke Schwartz <brookedschwartz@gmail.com>
2023-03-09 09:59:41 -0500

    hi

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -71,6 +71,20 @@ class GPT():
 
         response = completion.choices[0].text
         return response
+    
+    def brookeMethod(self, prompt):
+        ''' Generate a GPT response '''
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=f"Say what this program does: {prompt}",
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
+        response = completion.choices[0].text
+        return response
 
 if __name__=='__main__':
     '''
