096bd63aef538e9b5ad45524ae533d85aa8455c7
Simon Huynh <66090175+sthuynh95@users.noreply.github.com>
2023-03-26 18:09:47 -0400

    test for cat

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -73,9 +73,14 @@ def test_sum_by_date():
                                                           {'item #': 1, 'amount': 11.0, 'category': 'food', 'date': '2019-01-01', 'description': 'test'},
                                                           {'item #': 7, 'amount': 7.0, 'category': 'supply', 'date': '2019-05-19', 'description': 'Bread'}]
 
-# def test_sum_by_category():
-#    '''test the sum_by_category method'''
-#    transaction_list = Transaction()
+def test_sum_by_category():
+   '''test the sum_by_category method'''
+   transaction_list = Transaction()
+   
+   assert transaction_list.sum_by_category() == [{'item #': 1, 'amount': 11.0, 'category': 'food', 'date': '2019-01-01', 'description': 'test'},
+                                                  {'item #': 6, 'amount': 6.74, 'category': 'food', 'date': '2010-05-27', 'description': 'Pens'},
+                                                  {'item #': 4, 'amount': 4.74, 'category': 'supply', 'date': '2010-02-27', 'description': 'Pens'},
+                                                  {'item #': 7, 'amount': 7.0, 'category': 'supply', 'date': '2019-05-19', 'description': 'Bread'}]
 
 
 def test_delete_all():
