de591d81810d86f8083f2c7e9f6be6cc1f21cebc
Simon H <sthuynh95@brandeis.edu>
2023-03-26 15:47:04 -0400

    some edits

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -5,10 +5,12 @@ from transaction import Transaction
 def test_add():
     '''test the add method'''
     transaction_list = Transaction()
-    transaction = {'amount':1,'category':'food','date':'2019-01-01',
+    transaction = {'amount':11,'category':'food','date':'2019-01-01',
                         'description': 'test'}
     transaction_list.add(transaction)
-    assert transaction_list.select_active()[-1]['amount'] == 1
+    # note that transaction_list.select_active() is a list of dictionaries
+    # transaction_list.select_active()[-1] is the last transaction in the list
+    assert transaction_list.select_active()[-1]['amount'] == 11
     assert transaction_list.select_active()[-1]['category'] == 'food'
     assert transaction_list.select_active()[-1]['date'] == '2019-01-01'
     assert transaction_list.select_active()[-1]['description'] == 'test'
