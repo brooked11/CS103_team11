a6b123c04ab0ebbed949084dd689451b9680b48f
Click41 <annakolb@brandeis.edu>
2023-04-10 11:12:01 -0400

    adding trnasaction

--- pa04/firstapp/routes/transaction.js
+++ pa04/firstapp/routes/transaction.js
@@ -13,4 +13,38 @@ isLoggedIn = (req,res,next) => {
     }
   }
 
-  
+  router.get('/transaction/',
+  isLoggedIn,
+  async (req, res, next) => {
+      const show = req.query.show
+      const completed = show=='completed'
+      let items=[]
+      if (show) { // show is completed or todo, so just show some items
+        items = 
+          await ToDoItem.find({userId:req.user._id, completed})
+                        .sort({completed:1,priority:1,createdAt:1})
+      }else {  // show is null, so show all of the items
+        items = 
+          await ToDoItem.find({userId:req.user._id})
+                        .sort({completed:1,priority:1,createdAt:1})
+
+      }
+            res.render('toDoList',{items,show,completed});
+});
+
+
+
+/* add the value in the body to the list associated to the key */
+router.post('/transaction',
+  isLoggedIn,
+  async (req, res, next) => {
+      const todo = new ToDoItem(
+        {item:req.body.item,
+         createdAt: new Date(),
+         completed: false,
+         priority: parseInt(req.body.priority),
+         userId: req.user._id
+        })
+      await todo.save();
+      res.redirect('/transaction')
+});
